948db9fd3d3e25d878ad1c027e8816e8ca597319
Erik Nordin <aeketn@gmail.com>
2018-12-16 23:18:27 -0800

    Solved Day03 Part01

---
+++ day03/part01/src/main.rs
@@ -0,0 +1,42 @@
+use std::collections::HashMap;
+const PUZZLE_INPUT: &str = include_str!("../../input");
+
+type Point = (usize,usize);
+
+fn parse_claim(claim: &str) -> Vec<usize> {
+    claim.split(|c: char| !c.is_digit(10))
+        .filter(|s| !s.is_empty())
+        .skip(1)
+        .map(|num| num.parse::<usize>().unwrap())
+        .collect()
+}
+
+fn squares(claim: Vec<usize>) -> Vec<Point> {
+    let x_start = claim[0];
+    let y_start = claim[1];
+    let x_dim   = claim[2];
+    let y_dim   = claim[3];
+    let mut squares = Vec::with_capacity(x_dim * y_dim);
+    for x in x_start..x_start + x_dim {
+        for y in y_start..y_start+y_dim {
+            squares.push((x,y));
+        }
+    }
+    squares
+}
+
+fn main() {
+    let squares = PUZZLE_INPUT
+        .lines()
+        .map(parse_claim)
+        .map(squares)
+        .fold(HashMap::new(), |mut claims, claim| {
+            for square in claim {
+                claims.entry(square)
+                    .and_modify(|count| *count += 1)
+                    .or_insert(1);
+            }
+            claims
+        });
+    println!("{}", squares.iter().filter(|&(_,&count)| count > 1).count());
+}
